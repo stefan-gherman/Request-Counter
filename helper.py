c9439b030cb9fad5e1bbbcad954028e6f3ec3ee6
Stefan Gherman <scgheman@gmail.com>
2019-12-22 00:51:30 +0200

    Add ability to view requests.

--- helper.py
+++ helper.py
@@ -1,3 +1,8 @@
 
 methods = ['GET', 'POST', 'PUT', 'DELETE']
 
+def sort_dict(dicto):
+    items = dicto.items()
+    sorted_items = sorted(items , key =lambda item: item[1], reverse=True)
+    sorted_dict = dict(sorted_items)
+    return sorted_dict
--- server.py
+++ server.py
@@ -1,22 +1,38 @@
 from flask import Flask, request, render_template, redirect, url_for
 import helper as helper
+
 app = Flask(__name__)
 
 method_count = {'GET': 0, 'POST': 0, 'PUT': 0, 'DELETE': 0}
-@app.route('/', methods = ['GET', 'POST', 'DELETE', 'PUT'])
+
+
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def go_home():
+
     return render_template('index.html')
 
 
 @app.errorhandler(404)
-def return_404(string = 'string'):
+def return_404(string='string'):
     return render_template('404_handler.html')
+
 
 @app.route('/statistics')
 def return_stats():
-    return render_template('stats.html', method_count = method_count)
+    return render_template('stats.html', method_count=method_count)
 
+@app.route('/request-counter', methods=['GET', 'POST', 'DELETE', 'PUT'])
+def count_req():
+    global method_count
+    if request.method in method_count.keys():
+        method_count[request.method] += 1
+        method_count = helper.sort_dict(method_count)
+        print(request.method, method_count[request.method])
+    return redirect(request.referrer)
 if __name__ == '__main__':
     app.run(debug=True,
             host='0.0.0.0',
             port=7000)
+
+
